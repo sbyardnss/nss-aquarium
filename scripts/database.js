df67b36458ab77967510a58637cfc067f2b7ee9d
Mary Williams <sbyardnss@gmail.com>
2023-01-12 16:03:53 -0600

    finished automated page

--- scripts/database.js
+++ scripts/database.js
@@ -5,24 +5,28 @@ const database = {
             species: "Pike",
             food: "fish",
             size: 3,
+            image: "pictures/fish/download (1).jpeg",
         },
         {
             name: "Bev",
             species: "Clownfish",
             food: "algae",
             size: .1,
+            image: "pictures/fish/clownfish.jpeg",
         },
         {
             name: "Inigo Montoya",
             species: "Sunfish",
             food: "fish",
             size: 6,
+            image: "pictures/fish/sunfish.jpeg",
         },
         {
             name: "Lilo",
             species: "Catfish",
             food: "fish",
             size: 2,
+            image: "pictures/fish/catfish.jpeg"
         }
     ]
 };
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 import { getFish } from './database.js'
-
 const allFish = getFish()
 
 for (const fish of allFish) {
@@ -7,3 +6,17 @@ for (const fish of allFish) {
 }
 
 
+// Import the FishList function from the correct module
+import { FishList } from './fishlist.js'
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector(".fishTank")
+
+
+parentHTMLElement.innerHTML = FishList()
